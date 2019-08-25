a9ea8ca4e4009d47dc5262d9847e0dae42cc8c0b
Jeeka Tursunbai <zt9074@gmail.com>
2019-08-24 20:08:14 -0700

    All running

--- src/main/java/core/Function.java
+++ src/main/java/core/Function.java
@@ -1,90 +1,202 @@
 package core;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.concurrent.TimeUnit;
-import org.openqa.selenium.*;
-import org.openqa.selenium.safari.*;
 
-
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Function {
-
+	
+	
 	static WebDriver driver;
 	static String url;
-	static long start;
-	static long finish;
+	static Writer report;
 	
 	public static void open(String url) {
 		Logger.getLogger("").setLevel(Level.OFF);
 		driver = new SafariDriver();
+		driver.manage().timeouts().implicitlyWait(2,  TimeUnit.SECONDS);
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 		driver.get(url);
+	}
+
+static boolean isElementPresent(By by) {
 		
+		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		if (!driver.findElements(by).isEmpty()) return true; else return false;
+	}
+	
+
+static String getSize(By by) {
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		if (!driver.findElements(by).isEmpty() && driver.findElement(by).isDisplayed())
+			// return driver.findElement(by).getRect().getDimension()
+			return driver.findElement(by).getSize().toString().replace(", ", "x"); else return "null";
+	}
+	
+	static String getLocation(By by) {
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		if (((RemoteWebDriver) driver).getCapabilities().getBrowserName().equals("Safari")) return "0x0";
+		
+		if (!driver.findElements(by).isEmpty() && driver.findElement(by).isDisplayed()) 
+			return driver.findElement(by).getRect().getPoint().toString().replace(", ",	"x");
+		else return "null";
+	}
+	
+	static void setValue(By by, String value) {
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		if (!driver.findElements(by).isEmpty() && driver.findElement(by).isDisplayed()) driver.findElement(by).sendKeys(value);;
 		
 	}
 	
-	public static boolean isPresent(By by) {
-		if (driver.findElements(by).size() > 0) return true; else return false;
+	static String getValue(By by) {
+		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		if (!driver.findElements(by).isEmpty() && driver.findElement(by).isDisplayed()	&&
+		driver.findElement(by).getTagName().equalsIgnoreCase("input")) 
+			return driver.findElement(by).getAttribute("value").toString().trim();
+		
+		else if (!driver.findElements(by).isEmpty() && driver.findElement(by).isDisplayed() &&
+				driver.findElement(by).getTagName().equalsIgnoreCase("span"))
+			return driver.findElement(by).getText().trim();
+			else return "null";
 	}
 	
-	public static boolean isVisible(By by) {
-		if ((driver.findElements(by).size() > 0) && driver.findElement(by).isDisplayed()) return true; else return false;
-	}
-	
-	static By e1 = By.id("id_quotes");
-	static By e2 = By.id("id_current_location");
-	static By e3 = By.id("id_weather_icon");
-	static By e4 = By.id("id_temperature");
-	static By e5 = By.id("id_f_title");
-	
-	
-	public static void pageSignUpValidation(String url) {
-		open(url);
-	
-		System.out.println("Page URL: " + driver.getCurrentUrl());
-		System.out.println("01.Element[Quotes]: " + (isPresent(e1) ? "Exists" : "Not exist"));  
-		System.out.println("02.Element[Location]: " + (isPresent(e2) ? "Exists" : "Not exist"));  
-		System.out.println("03.Element[Weather Icon]: " + (isPresent(e3) ? "Exists" : "Not exist"));  
-		System.out.println("04.Element[Temperature]: " + (isPresent(e4) ? "Exists" : "Not exist"));  
-		System.out.println("05.Element[Title]: " + (isPresent(e5) ? "Exists" : "Not exist"));  
-		driver.findElement(By.id("id_fname")).sendKeys("Jeeka");
-		driver.findElement(By.id("id_lname")).sendKeys("Tursunbai");
-		driver.findElement(By.id("id_email")).sendKeys("email@email.com");
-		driver.findElement(By.id("id_phone")).sendKeys("415-555-5556");
-		driver.findElement(By.id("id_submit_button")).click();
+	static void submit(By by) {
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		if (!driver.findElements(by).isEmpty() && driver.findElement(by).isDisplayed())
+			driver.findElement(by).submit();
 		
 	}
 	
-	static By e6 = By.id("id_f_label_fn");
-	static By e7 = By.id("id_fname");
-	static By e8 = By.id("id_f_label_ln");
-	static By e9 = By.id("id_lname");
-	static By e10 = By.id("id_f_label_ea");
 	
-	public static void pageConfirmationValidation() {
-		// open(url);
-		// driver.manage().timeouts().implicitlyWait(15,  TimeUnit.SECONDS);
-		
-		System.out.println("Page URL: " + driver.getCurrentUrl());
-		
-		System.out.println("06.Element[Fname Label]: " + (isPresent(e6) ? "Exists" : "Not exist"));  
-		System.out.println("07.Element[First Name]: " + (isPresent(e7) ? "Exists" : "Not exist"));  
-		System.out.println("08.Element[Lname Label]: " + (isPresent(e8) ? "Exists" : "Not exist"));  
-		System.out.println("09.Element[Last Name]: " + (isPresent(e9) ? "Exists" : "Not exist")); 
-		System.out.println("10.Element[Email label]: " + (isPresent(e10) ? "Exists" : "Not exists"));  //signup & confirm
-	}
 	
-	public static void main(String[] args)	{
-		System.out.println("Browser: Safari");
-		start = System.currentTimeMillis();
-		pageSignUpValidation("http://alex.academy/exe/signup/v1/index.php");
-		pageConfirmationValidation();
+	
+
+	static String getBrowser() {
 		
+		String browser = ((RemoteWebDriver) driver).getCapabilities().getBrowserName().toString().trim();
+		return browser.replaceFirst(String.valueOf(browser.charAt(0)), String.valueOf(browser.charAt(0)).toUpperCase());
+		// return browser.substring(0,1).toUpperCase() + browser.substring(1).toLowerCase();
+			}
+
+	static String getOS() {
+		return ((RemoteWebDriver) driver).getCapabilities().getPlatform().toString().trim();
+			}
+	
+	static String getFileName() {
+		String file = driver.getCurrentUrl().toString().trim();
+		return file.substring(file.lastIndexOf('/') + 1);
+			}
+	
+	static void waitTitlePage(String title){
+		WebDriverWait wait = new WebDriverWait(driver, 15); 
+		wait.until(ExpectedConditions.titleIs(title));
+		}
+	
+	static void writeReportHeader(Writer report) throws IOException {
+		report.write("#,Browser,Page,Field,isPresent,Value,Size,Location" + "\n");
+		System.out.print("#,Browser,Page,Field,isPresent,Value,Size,Location" + "\n");
+		}
+	
+static void writeReportLine(String index, String fieldName, By by, Writer report) throws IOException {
+		
+		report.write(
+				index + "," + 
+				getBrowser() + "," + 
+				getFileName() + "," + 
+				fieldName + "," + 
+				isElementPresent(by) + "," +
+				getValue(by) + "," +
+				getSize(by) + "," +
+				getLocation(by) + "\n");
+		
+	System.out.print(
+				index + "," + 
+				getBrowser() + "," + 
+				getFileName() + "," + 
+				fieldName + "," + 
+				isElementPresent(by) + "," +
+				getValue(by) + "," +
+				getSize(by) + "," +
+				getLocation(by) + "\n");
+		}
+	
+	static void quit(){
 		driver.quit();
-		finish = System.currentTimeMillis();
-		System.out.println("Response time: " + (finish - start) + " ms");
+		}
+	static By e1 = By.id("id_fname");
+	static By e2 = By.id("id_lname");
+	static By e3 = By.id("id_email");
+	static By e4 = By.id("id_phone");
+	static By e5 = By.id("id_submit_button");
+	
+	
+	
+	public static void main(String[] args) throws IOException {
+
+		open("http://alex.academy/exe/signup/v1/index.php");
+report = new FileWriter ("./report_" + getBrowser().toString().toLowerCase() + ".csv", false);  
+		
+		// open(url);
+		writeReportHeader(report);
+		
+		// First Name
+		setValue(e1, "Jeeka");
+		writeReportLine("01", "First Name", e1, report);
+		
+		// Last Name
+				setValue(e2, "Tursunbai");
+				writeReportLine("02", "Last Name", e2, report);
+				
+				// Email
+				setValue(e3, "email@test.com");
+				writeReportLine("03", "Email", e3, report);
+				
+				// Phone 
+				setValue(e4, "4155555555");
+				writeReportLine("04", "Phone", e4, report);
+				
+				// Submit
+				
+				submit(e5);
+				waitTitlePage("Confirmation");
+				
+				
+				// First Name
+				writeReportLine("05", "First Name", e1, report);
+				
+				// Last Name
+				writeReportLine("06", "Last Name", e2, report);
+				
+				// Email
+				writeReportLine("07", "Email", e3, report);
+				
+				// Phone	
+				writeReportLine("08", "Phone", e4, report);
+				
+		
+				report.flush();
+				report.close();
+				
+		quit();
+		
+		
+		
+	
+		
+		
 	}
 	
 }
--- src/main/java/core/Linear.java
+++ src/main/java/core/Linear.java
@@ -1,5 +1,76 @@
 package core;
 
-public class Linear {
+import java.util.concurrent.TimeUnit;
+import java.util.logging.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.safari.*;
 
+
+
+
+public class Linear {
+	
+	static WebDriver driver;
+	static String url;
+	static long start;
+	static long finish;
+	
+	public static void open(String url) {
+		Logger.getLogger("").setLevel(Level.OFF);
+		driver = new SafariDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get(url);
+		System.out.println("Page URL: " + driver.getCurrentUrl());
+	}
+
+	
+	public static boolean isPresent(By by) {
+		
+		if (driver.findElements(by).size() > 0) return true; else return false;
+	}
+	
+	public static boolean isVisible(By by) {
+		
+		if((driver.findElements(by).size() > 0) && driver.findElement(by).isDisplayed()) return true; else return false;
+	}
+	
+	static By e1 = By.id("id_fname");
+	static By e2 = By.id("id_lname");
+	static By e3 = By.id("id_email");
+	static By e4 = By.id("id_phone");
+	static By e5 = By.id("id_submit_button");
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Browser: Safari");
+	
+	start = System.currentTimeMillis();
+	url = "http://alex.academy/exe/signup/v1/index.php";
+	
+	open(url);
+	
+	System.out.println("01. Element [First Name] " + (isPresent(e1) ? "Exists" : "Not exists"));
+	System.out.println("01. Element [First Name] " + (isPresent(e2) ? "Exists" : "Not exists"));
+	System.out.println("01. Element [First Name] " + (isPresent(e3) ? "Exists" : "Not exists"));
+	System.out.println("01. Element [First Name] " + (isPresent(e4) ? "Exists" : "Not exists"));
+	System.out.println("01. Element [First Name] " + (isPresent(e5) ? "Exists" : "Not exists"));
+	
+	driver.quit();
+	
+	url = "http://alex.academy/exe/signup/v1/confirmation.php";
+	open(url);
+	
+	System.out.println("01. Element [First Name] " + (isVisible(e1) ? "Visible" : "Not visible"));
+	System.out.println("01. Element [First Name] " + (isVisible(e2) ? "Visible" : "Not visible"));
+	System.out.println("01. Element [First Name] " + (isVisible(e3) ? "Visible" : "Not visible"));
+	System.out.println("01. Element [First Name] " + (isVisible(e4) ? "Visible" : "Not visible"));
+	
+	driver.quit();
+	
+	finish = System.currentTimeMillis()	;
+	
+	System.out.println("Response time: " + (finish - start) + " ms");
+	}
+	
 }
